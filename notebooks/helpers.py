993f62776dd93288c8f8d272b68229be50f5b656
Sourcery AI <>
2022-06-13 02:37:28 +0000

    'Refactored by Sourcery'

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -4,18 +4,11 @@
 def read_data(file_path):
     tokens = []
     tags = []
-    
+
     tweet_tokens = []
     tweet_tags = []
     for line in open(file_path, encoding='utf-8'):
-        line = line.strip()
-        if not line:
-            if tweet_tokens:
-                tokens.append(tweet_tokens)
-                tags.append(tweet_tags)
-            tweet_tokens = []
-            tweet_tags = []
-        else:
+        if line := line.strip():
             token, tag = line.split()
             # Replace all urls with <URL> token
             # Replace all users with <USR> token
@@ -24,8 +17,14 @@ def read_data(file_path):
                 token = '<URL>'
             if token[0] == '@':
                 token = '<USR>'
-                       
+
             tweet_tokens.append(token)
             tweet_tags.append(tag)
-            
+
+        else:
+            if tweet_tokens:
+                tokens.append(tweet_tokens)
+                tags.append(tweet_tags)
+            tweet_tokens = []
+            tweet_tags = []
     return tokens, tags
